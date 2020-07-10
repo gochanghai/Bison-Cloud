cbdb7985d4ebd56887b6f068dec8078420f92b5f
gochanghai <gochanghai@aliyun.com>
2020-07-11 00:01:30 +0800

    添加 gateway

---
+++ bison-oauth2-gateway/src/main/java/com/bison/gateway/Oauth2GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.bison.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class Oauth2GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2GatewayApplication.class, args);
+    }
+
+}
