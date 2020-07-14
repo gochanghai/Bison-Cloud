69948f51d97e312e1ff67fce83f537249b149978
gochanghai <gochanghai@aliyun.com>
2020-07-14 22:35:16 +0800

    修改Map创建方式

--- bison-oauth2-auth/src/main/java/com/bison/auth/component/JwtTokenEnhancer.java
+++ bison-oauth2-auth/src/main/java/com/bison/auth/component/JwtTokenEnhancer.java
@@ -1,13 +1,13 @@
 package com.bison.auth.component;
 
 import com.bison.auth.domain.SecurityUser;
+import com.google.common.collect.Maps;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,7 +19,7 @@ public class JwtTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
-        Map<String, Object> info = new HashMap<>();
+        Map<String, Object> info = Maps.newHashMap();
         //把用户ID设置到JWT中
         info.put("id", securityUser.getId());
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(info);
