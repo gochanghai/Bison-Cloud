5776f045bb03be00660dd0749ca4cb9eeff0fd12
gochanghai <gochanghai@aliyun.com>
2020-07-28 23:13:24 +0800

    添加枚举

---
+++ bison-common/bison-common-core/src/main/java/com/bison/common/core/enums/UserStatusEnum.java
@@ -0,0 +1,28 @@
+package com.bison.common.core.enums;
+
+/**
+ * 用户状态
+ *
+ * @author Changhai
+ */
+public enum UserStatusEnum {
+    OK("0", "正常"),
+    DISABLE("1", "停用"),
+    DELETED("2", "删除");
+
+    private final String code;
+    private final String info;
+
+    UserStatusEnum(String code, String info) {
+        this.code = code;
+        this.info = info;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+}
