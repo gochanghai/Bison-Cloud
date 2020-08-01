068533f7ceba87b66f5c9eb309f64c06440bbf09
gochanghai <gochanghai@aliyun.com>
2020-08-01 23:16:38 +0800

    添加枚举

---
+++ bison-modules/bison-cis/src/main/java/com/bison/cis/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.bison.api.controller;
+
+import com.bison.api.domain.UserDTO;
+import com.bison.api.holder.LoginUserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 获取登录用户信息接口
+ * Created by macro on 2020/6/19.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController{
+
+    @Autowired
+    private LoginUserHolder loginUserHolder;
+
+    @GetMapping("/currentUser")
+    public UserDTO currentUser() {
+        return loginUserHolder.getCurrentUser();
+    }
+
+}
