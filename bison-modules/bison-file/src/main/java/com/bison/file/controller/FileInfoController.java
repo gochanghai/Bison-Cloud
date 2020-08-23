dc8382fb22987f6ed86ea808bb7af2f0910801e7
gochanghai <gochanghai@aliyun.com>
2020-08-23 23:58:20 +0800

    添加 FileController

---
+++ bison-modules/bison-file/src/main/java/com/bison/file/controller/FileInfoController.java
@@ -0,0 +1,15 @@
+package com.bison.file.controller;
+
+import com.bison.common.core.web.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: Changhai.liu
+ * @date: 2020/8/23 23:56
+ */
+@RestController
+@RequestMapping("/sysRole")
+public class FileInfoController extends BaseController {
+}
