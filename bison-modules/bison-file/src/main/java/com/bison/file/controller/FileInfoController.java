c2939021e15efeace019ad0d7006543b2bcf071d
gochanghai <gochanghai@aliyun.com>
2020-08-29 23:56:19 +0800

    添加接口

--- bison-modules/bison-file/src/main/java/com/bison/file/controller/FileInfoController.java
+++ bison-modules/bison-file/src/main/java/com/bison/file/controller/FileInfoController.java
@@ -1,8 +1,12 @@
 package com.bison.file.controller;
 
 import com.bison.common.core.web.controller.BaseController;
+import com.bison.common.core.web.domain.CommonResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @description:
@@ -10,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2020/8/23 23:56
  */
 @RestController
-@RequestMapping("/sysRole")
+@RequestMapping("/fileInfo")
 public class FileInfoController extends BaseController {
+
+    /**
+     *
+     * @param param
+     * @return
+     */
+    @GetMapping("/list")
+    public CommonResult list(Map<String, Object> param) {
+        return CommonResult.success();
+    }
 }
