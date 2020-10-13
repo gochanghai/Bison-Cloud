16113a79ce868bff0758af70d1c71f820a64fdc4
gochanghai <gochanghai@aliyun.com>
2020-10-13 23:23:57 +0800

    添加

--- bison-visuals/bison-hystrix/src/main/java/com/ruoyi/modules/monitor/RuoYiMonitorApplication.java
+++ bison-visuals/bison-hystrix/src/main/java/com/ruoyi/modules/monitor/RuoYiMonitorApplication.java
@@ -1,6 +1,5 @@
 package com.ruoyi.modules.monitor;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
