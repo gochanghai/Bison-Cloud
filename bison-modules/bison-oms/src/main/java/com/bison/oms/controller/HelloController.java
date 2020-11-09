f31f0c671e340ecb3ddf34f4cd1f021abf640dc3
gochanghai@aliyun.com <gochanghai@aliyun.com>
2020-11-09 23:15:47 +0800

    edit

--- bison-modules/bison-oms/src/main/java/com/bison/oms/controller/HelloController.java
+++ bison-modules/bison-oms/src/main/java/com/bison/oms/controller/HelloController.java
@@ -1,6 +1,5 @@
-package com.bison.api.controller;
+package com.bison.oms.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,10 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World.";
-    }
 
 }
