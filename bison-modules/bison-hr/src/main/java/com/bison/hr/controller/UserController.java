80c3707d94578efe161de8f6ec604d222783d0a8
gochanghai@aliyun.com <gochanghai@aliyun.com>
2020-11-26 23:51:58 +0800

    delete

--- bison-modules/bison-hr/src/main/java/com/bison/hr/controller/UserController.java
+++ bison-modules/bison-hr/src/main/java/com/bison/hr/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.bison.hr.controller;
 
-import com.bison.api.holder.LoginUserHolder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController{
-
-    @Autowired
-    private LoginUserHolder loginUserHolder;
 
 }
